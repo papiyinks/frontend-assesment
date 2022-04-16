594b23f331b45759e9fe2dd7c9fc864c84d00fe0
Yinka Adeniyi <48151917+papiyinks@users.noreply.github.com>
2022-04-16 11:42:09 +0100

    testing

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,20 @@
 import React from "react";
+import { Routes, Route } from "react-router-dom";
+
+import Home from "./containers/Home/Home";
+import ImageSearch from "./containers/NasaImageSearch/NasaImageSearch";
+import ShowAMovie from "./containers/NasaImdbMovies/ShowAMovie/ShowAMovie";
+import Layout from "./Layout/Layout";
 
 const App = () => {
   return (
-    <h1 className="text-center">LumiQualis Technical Assigment</h1>
+    <Layout>
+      <Routes>
+        <Route path="/" element={ <Home /> } />
+        <Route path="/search" element={ <ImageSearch /> } />
+        <Route path="/movies/:id" element={ <ShowAMovie /> } />
+      </Routes>
+    </Layout>
   )
 }
 
--- src/index.tsx
+++ src/index.tsx
@@ -1,16 +1,39 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import { createStore, combineReducers, applyMiddleware, compose } from 'redux';
+import thunk from 'redux-thunk';
+
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+
+import nasaApiDataReducer from './redux-store/reducers/nasaApi';
+import nasaImdbDataReducer from './redux-store/reducers/imdbApi';
+
+const rootReducer = combineReducers({
+  nasaApiData: nasaApiDataReducer,
+  imdbApiData: nasaImdbDataReducer
+})
+
+export type RootState = ReturnType<typeof rootReducer>
+
+const composeEnhancers = (window as any).__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+const store = createStore(rootReducer, composeEnhancers(applyMiddleware(thunk)));
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <Provider store={store}>
+    <BrowserRouter>
+      <React.StrictMode>
+        <App />
+      </React.StrictMode>
+    </BrowserRouter>
+  </Provider>
 );
 
 // If you want to start measuring performance in your app, pass a function
